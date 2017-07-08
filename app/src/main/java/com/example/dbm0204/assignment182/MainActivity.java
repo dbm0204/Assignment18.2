bf4bf15b726e6751f4e5cf1652ddf7692cc846f5
dbm0204 <dbm0204@gmail.com>
2017-07-09 01:03:15 +0530

    Comments and Screenshots added

---
+++ app/src/main/java/com/example/dbm0204/assignment182/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dbm0204.assignment182;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The MainActivity extends AppCompactActivity and inflates activity_main
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
